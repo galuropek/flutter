c912c0d9ddf2a33814e8d9592fc18b48928297e3
galuropek <galuropek@gmail.com>
2020-05-24 14:55:47 +0300

    Added stream provider

--- first_app/lib/screens/home/home.dart
+++ first_app/lib/screens/home/home.dart
@@ -1,25 +1,33 @@
+import 'package:first_app/screens/home/record_list.dart';
 import 'package:first_app/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:first_app/services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Home extends StatelessWidget {
-
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: Text('Welcome to Home page ;)'),
-        backgroundColor: Colors.lightGreen,
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () async {
-                await _auth.signOut();
-              }, icon: Icon(Icons.person), label: Text('logout'))
-        ],
-      ),
-    );
+    return StreamProvider<QuerySnapshot>.value(
+        value: DatabaseService().records,
+        child: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('Welcome to Home page ;)'),
+            backgroundColor: Colors.lightGreen,
+            elevation: 0.0,
+            actions: <Widget>[
+              FlatButton.icon(
+                  onPressed: () async {
+                    await _auth.signOut();
+                  },
+                  icon: Icon(Icons.person),
+                  label: Text('logout'))
+            ],
+          ),
+          body: RecordList(),
+        ));
   }
 }
