72ac4b9a2223c33c87efe53b95419a12ad1d1809
galuropek <galuropek@gmail.com>
2020-05-24 15:40:31 +0300

    Created RecordTile widget.

---
+++ first_app/lib/screens/home/record_tile.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/models/record.dart';
+
+class RecordTile extends StatelessWidget {
+  final Record record;
+
+  RecordTile({this.record});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 8.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 25.0,
+            backgroundColor: Colors.brown[record.strength],
+          ),
+          title: Text(record.name),
+          subtitle: Text('Takes ${record.sugar} sugar(s)'),
+        ),
+      ),
+    );
+  }
+}
