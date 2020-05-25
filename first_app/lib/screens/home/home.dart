2e02a4f87068ad88b2555d69353fcb60f47b490a
galuropek <galuropek@gmail.com>
2020-05-25 19:19:10 +0300

    Added settings

--- first_app/lib/screens/home/home.dart
+++ first_app/lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:first_app/models/record.dart';
 import 'package:first_app/screens/home/record_list.dart';
+import 'package:first_app/screens/home/settings_from.dart';
 import 'package:first_app/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:first_app/services/database.dart';
@@ -10,6 +11,16 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    void _showSettingsPanel() {
+      showModalBottomSheet(context: context, builder: (context) {
+        return Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+          child: SettingsForm(),
+        );
+      });
+    }
+
     return StreamProvider<List<Record>>.value(
         value: DatabaseService().records,
         child: Scaffold(
@@ -24,7 +35,13 @@ class Home extends StatelessWidget {
                     await _auth.signOut();
                   },
                   icon: Icon(Icons.person),
-                  label: Text('logout'))
+                  label: Text('logout')),
+              FlatButton.icon(
+                  onPressed: () {
+                    _showSettingsPanel();
+                  },
+                  icon: Icon(Icons.settings),
+                  label: Text('settings'))
             ],
           ),
           body: RecordList(),
