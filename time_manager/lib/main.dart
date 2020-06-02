207bee4100aa6eb0cb84d3785a88da17171b0b75
galuropek <galuropek@gmail.com>
2020-06-02 22:40:10 +0300

    Stopwatch class practice: timer

--- time_manager/lib/main.dart
+++ time_manager/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,50 +25,94 @@ class TimeManager extends StatefulWidget {
 }
 
 class _TimeManagerState extends State<TimeManager> {
-  final Stopwatch stopwatch = new Stopwatch();
-  var time = 0;
-  var current_time_in_seconds = 0;
+  Timer _timer;
+  int _start = 0;
+
+
+  void startTimer() {
+    const oneSec = const Duration(seconds: 1);
+    _timer = new Timer.periodic(
+        oneSec,
+            (Timer timer) => setState(() {
+          if (_start > 60) {
+            timer.cancel();
+          } else {
+            _start = _start + 1;
+          }
+        }));
+  }
+
+
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          Text('time_var: $time'),
-          Text('current_time_in_seconds_var: $current_time_in_seconds'),
-          RaisedButton(
-              onPressed: () {
-                setState(() {
-                  time = stopwatch.elapsedMicroseconds;
-                });
-              },
-              child: Text('Show time')),
-          RaisedButton(
-            onPressed: () {
-              setState(() {
-                stopwatch.start();
-              });
-            },
-            child: Text('Start stopwatch'),
-          ),
-          RaisedButton(
-            onPressed: () {
-              setState(() {
-                stopwatch.stop();
-              });
-            },
-            child: Text('Stop stopwatch'),
-          ),
-          RaisedButton(
-            onPressed: () {
-              setState(() {
-                current_time_in_seconds = stopwatch.elapsed.inSeconds;
-              });
-            },
-            child: Text('Current time in seconds'),
-          )
-        ],
-      ),
-    );
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
   }
+
+
+
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: AppBar(title: Text("Timer test")),
+        body: Column(
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                startTimer();
+              },
+              child: Text("start"),
+            ),
+            Text("$_start")
+          ],
+        ));
+  }
+
+//  final Stopwatch stopwatch = new Stopwatch();
+//  var time = 0;
+//  var currentTimeInSeconds = 0;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Center(
+//      child: Column(
+//        children: <Widget>[
+//          Text('time_var: $time'),
+//          Text('current_time_in_seconds_var: $currentTimeInSeconds'),
+//          RaisedButton(
+//              onPressed: () {
+//                setState(() {
+//                  time = stopwatch.elapsedMicroseconds;
+//                });
+//              },
+//              child: Text('Show time')),
+//          RaisedButton(
+//            onPressed: () {
+//              setState(() {
+//                stopwatch.start();
+//              });
+//            },
+//            child: Text('Start stopwatch'),
+//          ),
+//          RaisedButton(
+//            onPressed: () {
+//              setState(() {
+//                stopwatch.stop();
+//              });
+//            },
+//            child: Text('Stop stopwatch'),
+//          ),
+//          RaisedButton(
+//            onPressed: () {
+//              setState(() {
+//                currentTimeInSeconds = stopwatch.elapsed.inSeconds;
+//              });
+//            },
+//            child: Text('Current time in seconds'),
+//          ),
+//
+//        ],
+//      ),
+//    );
+//  }
 }
