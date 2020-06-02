358e2dbfb7e563a2631b6d3d26760303404172e9
galuropek <galuropek@gmail.com>
2020-06-02 23:14:38 +0300

    Stopwatch class practice: timer

--- time_manager/lib/main.dart
+++ time_manager/lib/main.dart
@@ -24,50 +24,93 @@ class TimeManager extends StatefulWidget {
   _TimeManagerState createState() => _TimeManagerState();
 }
 
-class _TimeManagerState extends State<TimeManager> {
-  Timer _timer;
-  int _start = 0;
+class _TimeManagerState extends State<TimeManager>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
 
-
-  void startTimer() {
-    const oneSec = const Duration(seconds: 1);
-    _timer = new Timer.periodic(
-        oneSec,
-            (Timer timer) => setState(() {
-          if (_start > 60) {
-            timer.cancel();
-          } else {
-            _start = _start + 1;
-          }
-        }));
+  String get timerString {
+    Duration duration = _controller.duration * _controller.value;
+    return '${duration.inMinutes}:${duration.inSeconds}';
   }
-
-
 
   @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 20));
   }
 
-
-
+  @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: AppBar(title: Text("Timer test")),
-        body: Column(
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () {
-                startTimer();
-              },
-              child: Text("start"),
-            ),
-            Text("$_start")
-          ],
-        ));
+    return Column(
+      children: <Widget>[
+        AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return Text(
+                timerString,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              );
+            }),
+        RaisedButton(onPressed: () {
+          if (_controller.isAnimating) {
+            _controller.stop();
+          } else {
+            _controller.reverse(
+              from: _controller.value == 0.0 ? 1.0 : _controller.value
+            );
+          }
+        }, child: Text('On/Off'),)
+      ],
+    );
   }
-
+//  ##### step2 ########################
+//  Timer _timer;
+//  int _start = 0;
+//
+//
+//  void startTimer() {
+//    const oneSec = const Duration(seconds: 1);
+//    _timer = new Timer.periodic(
+//        oneSec,
+//            (Timer timer) => setState(() {
+//          if (_start > 60) {
+//            timer.cancel();
+//          } else {
+//            _start = _start + 1;
+//          }
+//        }));
+//  }
+//
+//
+//
+//  @override
+//  void dispose() {
+//    _timer.cancel();
+//    super.dispose();
+//  }
+//
+//
+//
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//        appBar: AppBar(title: Text("Timer test")),
+//        body: Column(
+//          children: <Widget>[
+//            RaisedButton(
+//              onPressed: () {
+//                startTimer();
+//              },
+//              child: Text("start"),
+//            ),
+//            Text("$_start")
+//          ],
+//        ));
+//  }
+// ###### step 1 #######################
 //  final Stopwatch stopwatch = new Stopwatch();
 //  var time = 0;
 //  var currentTimeInSeconds = 0;
